cc80224f949468d696bd73fc04494ec84f20b3ce
skcruzer <96440269+skcruzer@users.noreply.github.com>
2022-05-29 15:21:36 -0700

    removed controllers, create mutations, queries

---
+++ client/src/utils/queries.js
@@ -0,0 +1,20 @@
+import { gql } from '@apollo/client'
+
+// execute the me query set up using Apollo Server
+export const QUERY_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }
+    }
+  }
+`
